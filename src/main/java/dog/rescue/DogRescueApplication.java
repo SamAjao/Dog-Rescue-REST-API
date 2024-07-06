09f51b5689a6d2df1f0a6a020a92d2fdc034413a
unknown <samuel.ajao@gmail.com>
2024-07-06 15:38:11 -0700

    Created maven project files

---
+++ src/main/java/dog/rescue/DogRescueApplication.java
@@ -0,0 +1,21 @@
+/*
+// Program: Promineo Tech Backend Java Back End Development Course
+// Author:  Samuel Ajao
+// Subject:  Spring Boot API Project - Dog Rescue
+// Create Date: July 6, 2024
+//
+*/
+package dog.rescue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogRescueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogRescueApplication.class, args);
+
+	}
+
+}
